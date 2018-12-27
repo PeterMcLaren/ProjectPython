ef82988109846ef597ffc21ab0e6cbeabfb98988
Peter McLaren <13315640+PeterMcLaren@users.noreply.github.com>
2018-12-27 20:28:50 +0000

    exclaim

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,3 @@
 msg = "Hello world"
 for test in range(0,100,10):
-    print("the value is "+str(test)+" in case you were wondering")
+    print("the value is "+str(test)+" in case you were wondering!!!!")
