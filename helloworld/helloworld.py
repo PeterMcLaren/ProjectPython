8a0a1a4e87f55226d51e4e13b72263cfa5fdfd26
Peter McLaren <13315640+PeterMcLaren@users.noreply.github.com>
2019-01-05 11:55:49 +0000

    more function stuff

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,3 +1,22 @@
 msg = "Hello world"
 for test in range(0,100,10):
     print("the value is "+str(test)+" in case you were wondering!!")
+class Dog:
+    # Class Attribute
+    species='mammal'
+    # initializer
+    def __init__(self, name,age):
+        self.name=name
+        self.age=age
+a = Dog("fido",4)
+b = Dog("test",5)
+c = Dog("blah",6)
+print(type(a))
+print(a.age)
+print("{} is {} years old".format(a.name,a.age))
+
+def get_biggest_number(*args):
+    print(type(args))
+    return max(args)
+
+print("the oldest dog is {}".format(get_biggest_number(a.age,b.age,c.age)))
