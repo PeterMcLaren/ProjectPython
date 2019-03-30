8d5b1245ace50b35439664247dab29e958a2e01e
Peter McLaren <13315640+PeterMcLaren@users.noreply.github.com>
2019-03-30 19:27:45 +0000

    Works Amazingly!

--- helloworld/maze.py
+++ helloworld/maze.py
@@ -1,3 +1,8 @@
+import random
+
+#use tuple because immutable so faster
+direction=((1,0),(-1,0),(0,-1),(0,1))
+
 class block:
     #Class Attribute
     def __init__(self, colour, solid):
@@ -6,12 +11,13 @@ class block:
 
 def displaygrid(grid):
     for i in range(0,len(grid)):
-        for j in range(0,len(grid[i])):
-            if grid[i][j].solid:
-                print("X",end="")
-            else:
-                print(" ",end="")
-        print("")
+        for _ in range(2):
+            for j in range(0,len(grid[i])):
+                if grid[i][j].solid:
+                    print("XXXX",end="")
+                else:
+                    print("    ",end="")
+            print("")
 
 def creategrid(x,y,colour,solid):
     grid=[]
@@ -22,7 +28,25 @@ def creategrid(x,y,colour,solid):
         grid.append(x)
     return grid
 
-mazesize=20
-maze=creategrid(mazesize,mazesize,"black",True)
-maze[1][1].solid=False
+def gofromhere(x,y,grid):
+    tried=[False]*4
+    grid[x][y].solid=False
+    while False in tried:
+        d=random.randrange(4)
+        tried[d]=True
+        dx=direction[d][0]
+        dy=direction[d][1]
+        if checkifsolid(x+dx*2,y+dy*2,grid):
+            grid[x+dx][y+dy].solid=False
+            gofromhere(x+dx*2,y+dy*2,grid)
+
+def checkifsolid(x,y,grid):
+    if x>0 and x<len(grid):
+        if y>0 and y<len(grid[x]):
+            return grid[x][y].solid
+    return False
+
+
+maze=creategrid(21,41,"black",True)
+gofromhere(1,1,maze)
 displaygrid(maze)
