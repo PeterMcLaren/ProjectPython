7ca3867a78623bf48750b1b4608d9020368dc493
Peter McLaren <13315640+PeterMcLaren@users.noreply.github.com>
2019-05-18 17:09:36 +0100

    added static page

--- helloworld/sqrnote.py
+++ helloworld/sqrnote.py
@@ -1,6 +1,6 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='',static_folder='')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return app.send_static_file("sqrpage.html")
